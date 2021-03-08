80c3a846776809d896eede4a6b93b30b7178fb28
Juan Alfaro <alfaro.j@husky.neu.edu>
2021-03-08 13:37:01 -0500

    Adds services for lesson, modules, and topics

---
+++ src/services/module-service.js
@@ -0,0 +1,43 @@
+import {NEU_USERNAME} from "./course-service";
+
+const COURSES_URL = `https://wbdv-generic-server.herokuapp.com/api/${NEU_USERNAME}/courses`
+const MODULES_URL = `https://wbdv-generic-server.herokuapp.com/api/${NEU_USERNAME}/modules`
+
+export const createModule = (courseId, module) =>
+    fetch(`${COURSES_URL}/${courseId}/modules`, {
+        method: "POST",
+        body: JSON.stringify(module),
+        headers: {
+            'content-type': 'application/json'
+        }
+    })
+        .then(response => response.json());
+
+export const updateModule = (moduleId, module) =>
+    fetch(`${MODULES_URL}/${moduleId}`, {
+        method: "PUT",
+        body: JSON.stringify(module),
+        headers: {
+            'content-type': 'application/json'
+        }
+    })
+        .then(response => response.json());
+
+export const findModulesForCourse = (courseId) =>
+    fetch(`${COURSES_URL}/${courseId}/modules`)
+        .then(response => response.json());
+
+export const deleteModule = (moduleId) =>
+    fetch(`${MODULES_URL}/${moduleId}`, {
+        method: 'DELETE'
+    })
+        .then(response => response.json());
+
+const api = {
+    createModule,
+    findModulesForCourse,
+    deleteModule,
+    updateModule
+}
+
+export default api;
