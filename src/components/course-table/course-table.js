4b079dd3743de17aa528710b06fd1ad34bc5114f
Juan Alfaro <alfaro.j@husky.neu.edu>
2021-03-08 13:26:14 -0500

    Adds key to course row in list and adds table row to header

--- src/components/course-table/course-table.js
+++ src/components/course-table/course-table.js
@@ -13,22 +13,24 @@ export default class CourseTable extends React.Component {
             <div className="container">
                 <table className="table">
                     <thead>
-                    <td>
-                        <h5>Course Name</h5>
-                    </td>
-                    <td className="d-none d-md-table-cell">
-                        <h5>Owned By</h5>
-                    </td>
-                    <td className="d-none d-lg-table-cell">
-                        <h5>Last Modified</h5>
-                    </td>
-                    <td>
-                        <i className="fas fa-2x fa-folder mx-2"/>
-                        <i className="fas fa-2x fa-sort-alpha-up-alt mx-2"/>
-                        <Link to="/courses/grid">
-                            <i className="fas fa-2x fa-th mx-2"/>
-                        </Link>
-                    </td>
+                    <tr>
+                        <td>
+                            <h5>Course Name</h5>
+                        </td>
+                        <td className="d-none d-md-table-cell">
+                            <h5>Owned By</h5>
+                        </td>
+                        <td className="d-none d-lg-table-cell">
+                            <h5>Last Modified</h5>
+                        </td>
+                        <td>
+                            <i className="fas fa-2x fa-folder mx-2"/>
+                            <i className="fas fa-2x fa-sort-alpha-up-alt mx-2"/>
+                            <Link to="/courses/grid">
+                                <i className="fas fa-2x fa-th mx-2"/>
+                            </Link>
+                        </td>
+                    </tr>
                     </thead>
                     <tbody>
                     {
@@ -37,6 +39,7 @@ export default class CourseTable extends React.Component {
                                 deleteCourse={this.props.deleteCourse}
                                 updateCourse={this.props.updateCourse}
                                 course={course}
+                                key={course._id}
                             />)
                     }
                     </tbody>
