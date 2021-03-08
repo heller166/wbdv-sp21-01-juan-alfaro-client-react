e5c74a3f70264cd660b243a34f00be93c179a172
Juan Alfaro <alfaro.j@husky.neu.edu>
2021-03-08 13:39:55 -0500

    Adds reducers for topics, modules, and lessons.

---
+++ src/reducers/topic-reducer.js
@@ -0,0 +1,43 @@
+const initialState = {
+    topics: []
+}
+
+const topicReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case "CREATE_TOPIC":
+            return {
+                ...state,
+                topics: [
+                    ...state.topics,
+                    action.topic
+                ]
+            }
+        case "DELETE_TOPIC":
+            return {
+                ...state,
+                topics: state.topics.filter(topic => {
+                    return topic._id !== action.topicToDelete._id;
+                })
+            }
+        case "UPDATE_TOPIC":
+            return {
+                ...state,
+                topics: state.topics.map(topic => {
+                    if(topic._id === action.updateTopic._id) {
+                        return action.updateTopic
+                    } else {
+                        return topic
+                    }
+                })
+            }
+        case "FIND_TOPICS_FOR_LESSON":
+            return {
+                ...state,
+                topics: action.topics
+            }
+        default:
+            return state
+    }
+}
+
+export default topicReducer
