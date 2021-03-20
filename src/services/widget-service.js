29af5775b03727096b460a2fd671ff415c407bcf
Juan Alfaro <alfaro.j@husky.neu.edu>
2021-03-20 12:34:48 -0400

    Adds widget service

---
+++ src/services/widget-service.js
@@ -0,0 +1,40 @@
+const WIDGETS_URL = `https://wbdv-sp21-03-juan-alfaro-serve.herokuapp.com/api`
+
+export const createWidget = (topicId, widget) =>
+    fetch(`${WIDGETS_URL}/topics/${topicId}/widgets`, {
+        method: "POST",
+        body: JSON.stringify(widget),
+        headers: {
+            'content-type': 'application/json'
+        }
+    })
+        .then(response => response.json());
+
+export const updateWidget = (widgetId, widget) =>
+    fetch(`${WIDGETS_URL}/widgets/${widgetId}`, {
+        method: "PUT",
+        body: JSON.stringify(widget),
+        headers: {
+            'content-type': 'application/json'
+        }
+    })
+        .then(response => response.json());
+
+export const findWidgetsForTopic = (topicId) =>
+    fetch(`${WIDGETS_URL}/topics/${topicId}/widgets`)
+        .then(response => response.json());
+
+export const deleteWidget = (widgetId) =>
+    fetch(`${WIDGETS_URL}/widgets/${widgetId}`, {
+        method: 'DELETE'
+    })
+        .then(response => response.json());
+
+const api = {
+    createWidget,
+    findWidgetsForTopic,
+    deleteWidget,
+    updateWidget
+}
+
+export default api;
